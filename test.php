612cb5fb3d74c3d5623735c6e32685e5100a21c2
liam4 <liamdraws4@gmail.com>
2015-02-06 10:27:43 -0400

    Automatic commit via Makefile

---
+++ test.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Test</title>
+    </head>
+    <body>
+        <?php
+
+        echo "If this doesn't work..<br />"
+        echo ".. I didn't expect it to."
+
+        ?>
+    </body>
