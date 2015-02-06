ae2aeee344c9fd2a8132c696a0154ad948d25058
liam4 <liamdraws4@gmail.com>
2015-02-06 08:41:43 -0400

    Automatic commit via Makefile

--- reference/program-include-demo.js
+++ reference/program-include-demo.js
@@ -5,8 +5,11 @@ var program = new Program(function() {
     // When we include a file, we need to wait for it to load before doing anything.
     // Best bet is to just wait 1.5 seconds.
     setTimeout(function() {
-        var pageHanlder = new PageHandler();
-        var targeter = pageHanlder.getElementTargeter({id: "demo"});
-        pageHandler.editElement(targeter, 'write', "This is an example text edited by a <code>new PageHandler()</code>!");
+        var ph = new PageHandler();
+        console.log("1");
+        var targeter = ph.getElementTargeter({id: "demo"});
+        console.log("2");
+        ph.editElement(targeter, 'write', "This is an example text edited by a <code>new PageHandler()</code>!");
+        console.log("3");
     }, 1500);
 });
