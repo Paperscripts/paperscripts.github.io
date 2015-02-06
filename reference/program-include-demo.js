188d9d15bc4dcb3ae762ca4b0afc6f1a8039a16f
liam4 <liamdraws4@gmail.com>
2015-02-06 08:11:12 -0400

    Automatic commit via Makefile

--- reference/program-include-demo.js
+++ reference/program-include-demo.js
@@ -3,12 +3,13 @@ var program = new Program(function() {
     console.log("Startup.");
     this.include("page-handle", "../paperscript-libs")
 
+    // When we include a file, we need to wait for it to load before doing anything.
+    // Best bet is to just wait two seconds.
     setTimeout(function() {
-        // Create a new page handler.
         var pageHanlder = new PageHandler();
         console.log(pageHanlder);
         var targeter = pageHanlder.getElementTargeter({id: "demo"});
         console.log(targeter);
         console.log("Success.");
-    }, 2000);
+    }, 0);
 });
