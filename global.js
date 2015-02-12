be0433a272d62de728bf1b4eedb243ca01594a42
liam4 <liamdraws4@gmail.com>
2015-02-12 19:47:06 -0400

    Automatic commit via Makefile

---
+++ global.js
@@ -0,0 +1,10 @@
+/** Global script for paperscripts.github.io **/
+
+function make_nav() {
+    var navbar = document.getElementById("nav");
+}
+
+function load() {
+    // To be run in <body onload>.
+    make_nav();
+}
