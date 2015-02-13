cacbada21db8e35bfcb11e448ad023fdf569586b
liam4 <liamdraws4@gmail.com>
2015-02-13 07:19:44 -0400

    Automatic commit via Makefile

--- global.js
+++ global.js
@@ -1,11 +1,15 @@
 /** Global script for paperscripts.github.io **/
 
 function make_nav() {
+    alert("Loading nav.");
     var navbar = document.getElementById("nav");
     var navbarfilegetter = new XMLHttpRequest();
     var navbarfilecontents = "";
+    alert("Created variables for loading nav");
     navbarfilegetter.open("GET", "navbar.xml", false);
+    alert("Opened file for nav");
     navbarfilecontents = navbarfilegetter.responseXML;
+    alert("Loaded text from file: " + navbarfilecontents);
 }
 
 function load() {
