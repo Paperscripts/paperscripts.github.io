a0a5321f56bdcd8a73aab2536602d6ec7178e316
liam4 <liamdraws4@gmail.com>
2015-02-13 07:28:58 -0400

    Automatic commit via Makefile

--- global.js
+++ global.js
@@ -1,15 +1,24 @@
 /** Global script for paperscripts.github.io **/
 
 function make_nav() {
-    alert("Loading nav.");
-    var navbar = document.getElementById("nav");
-    var navbarfilegetter = new XMLHttpRequest();
-    var navbarfilecontents = "";
-    alert("Created variables for loading nav");
-    navbarfilegetter.open("GET", "navbar.xml", false);
-    alert("Opened file for nav");
-    navbarfilecontents = navbarfilegetter.responseXML;
-    alert("Loaded text from file: " + navbarfilecontents);
+    function processingData(data) {
+        // Taking care of data.
+        alert("Got data '" + data + "'.");
+        document.getElementById("nav").innerHTML = data;
+    }
+
+    function handler() {
+        if (this.readyState == this.DONE) {
+            if (this.status == 200 &&
+                this.responseText != null &&
+                this.responseXML.getElementsByTagName("ul")[0].textContext) {
+                processData(
+                    this.responseXML
+                    .getElementsByTagName("ul")[0]
+                    .textContext);
+            }
+        }
+    }
 }
 
 function load() {
